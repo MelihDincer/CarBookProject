f510c3a8ece7b46a4b9ec3d7b10898ccc22c03a3
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-09 21:41:01 +0300

    GetAboutByIdQueryHandler sınıfı oluşturuldu.

---
+++ Core/CarBookProject.Application/Features/CQRS/Handlers/AboutHandlers/GetAboutByIdQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using CarBookProject.Application.Features.CQRS.Queries.AboutQueries;
+using CarBookProject.Application.Features.CQRS.Results.AboutResults;
+using CarBookProject.Application.Interfaces;
+using CarBookProject.Domain.Entities;
+
+namespace CarBookProject.Application.Features.CQRS.Handlers.AboutHandlers;
+
+public class GetAboutByIdQueryHandler
+{
+    private readonly IRepository<About> _repository;
+
+    public GetAboutByIdQueryHandler(IRepository<About> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<GetAboutByIdQueryResult> Handle(GetAboutByIdQuery query)
+    {
+        var values = await _repository.GetByIdAsync(query.Id);
+        return new GetAboutByIdQueryResult
+        {
+            AboutID = values.AboutID,
+            Description = values.Description,
+            ImageUrl = values.ImageUrl,
+            Title = values.Title
+        };
+    }
+}
