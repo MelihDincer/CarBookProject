0551fb147c2e387e33025a54c044bed72c381048
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-10 00:29:15 +0300

    About command handler ların yazılması.

---
+++ Core/CarBookProject.Application/Features/CQRS/Handlers/AboutHandlers/RemoveAboutCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using CarBookProject.Application.Features.CQRS.Commands.AboutCommands;
+using CarBookProject.Application.Interfaces;
+using CarBookProject.Domain.Entities;
+
+namespace CarBookProject.Application.Features.CQRS.Handlers.AboutHandlers;
+
+public class RemoveAboutCommandHandler
+{
+    private readonly IRepository<About> _repository;
+
+    public RemoveAboutCommandHandler(IRepository<About> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task Handle(RemoveAboutCommand command)
+    {
+        var value = await _repository.GetByIdAsync(command.Id);
+        await _repository.RemoveAsync(value);
+    }
+}
---
+++ Core/CarBookProject.Application/Features/CQRS/Handlers/AboutHandlers/UpdateAboutCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using CarBookProject.Application.Features.CQRS.Commands.AboutCommands;
+using CarBookProject.Application.Interfaces;
+using CarBookProject.Domain.Entities;
+
+namespace CarBookProject.Application.Features.CQRS.Handlers.AboutHandlers;
+
+public class UpdateAboutCommandHandler
+{
+    private readonly IRepository<About> _repository;
+
+    public UpdateAboutCommandHandler(IRepository<About> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task Handle(UpdateAboutCommand command)
+    {
+        var value = await _repository.GetByIdAsync(command.AboutID);
+        value.Description = command.Description;
+        value.ImageUrl = command.ImageUrl;
+        value.Title = command.Title;
+        await _repository.UpdateAsync(value);
+    }
+}
