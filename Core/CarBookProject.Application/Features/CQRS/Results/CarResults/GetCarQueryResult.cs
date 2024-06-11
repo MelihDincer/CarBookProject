98acfc347df887726084ce711ecbc8cd6c68cd34
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-11 12:49:03 +0300

    Car entity listeleme ve getirme işlemleri için Query Result sınıflarının oluşturulması.

---
+++ Core/CarBookProject.Application/Features/CQRS/Results/CarResults/GetCarQueryResult.cs
@@ -0,0 +1,15 @@
+﻿namespace CarBookProject.Application.Features.CQRS.Results.CarResults;
+
+public class GetCarQueryResult
+{
+    public int CarID { get; set; }
+    public int BrandID { get; set; }
+    public string Model { get; set; }
+    public string CoverImageUrl { get; set; }
+    public int Km { get; set; }
+    public string Transmission { get; set; }
+    public byte Seat { get; set; } //Koltuk
+    public byte Luggage { get; set; } //Bagaj
+    public string Fuel { get; set; } //Yakıt
+    public string BigImageUrl { get; set; } 
+}
