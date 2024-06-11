2b5c6ed1f982c9f17e32e69555cac27259de7fef
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-11 12:51:49 +0300

    Araba listesinde marka bilgisinin de yer alabilmesi için ilgili Query Handler sınıfı yazıldı.

---
+++ Core/CarBookProject.Application/Features/CQRS/Handlers/CarHandlers/GetCarWithBrandQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using CarBookProject.Application.Features.CQRS.Results.CarResults;
+using CarBookProject.Application.Interfaces;
+using CarBookProject.Application.Interfaces.CarInterfaces;
+using CarBookProject.Domain.Entities;
+
+namespace CarBookProject.Application.Features.CQRS.Handlers.CarHandlers;
+
+public class GetCarWithBrandQueryHandler
+{
+    private readonly ICarRepository _repository;
+
+    public GetCarWithBrandQueryHandler(ICarRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public List<GetCarWithBrandQueryResult> Handle()
+    {
+        var values = _repository.GetCarListWithBrands();
+        return values.Select(x => new GetCarWithBrandQueryResult
+        {
+            CarID = x.CarID,
+            BrandID = x.BrandID,
+            BrandName = x.Brand.Name,
+            Model = x.Model,
+            Seat = x.Seat,
+            Transmission = x.Transmission,
+            Fuel = x.Fuel,
+            Km = x.Km,
+            Luggage = x.Luggage,
+            CoverImageUrl = x.CoverImageUrl,
+            BigImageUrl = x.BigImageUrl
+        }).ToList();
+    }
+}
