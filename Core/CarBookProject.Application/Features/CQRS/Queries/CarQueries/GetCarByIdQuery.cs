9a88098d44d5d40718a367049de2b2291dd49f32
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-11 12:48:22 +0300

    Car entity için Id ye göre getirme işlemi için Query sınıfının oluşturulması.

---
+++ Core/CarBookProject.Application/Features/CQRS/Queries/CarQueries/GetCarByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿namespace CarBookProject.Application.Features.CQRS.Queries.CarQueries;
+
+public class GetCarByIdQuery
+{
+    public int Id { get; set; }
+    public GetCarByIdQuery(int id)
+    {
+        Id = id;
+    }
+}
