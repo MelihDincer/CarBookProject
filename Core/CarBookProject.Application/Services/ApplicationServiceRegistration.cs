d0ab720fc565d74ceba66c3b1b81046e8b2b5773
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-11 22:42:00 +0300

    Application katmanı için MediatR kütüphanesinden yararlanılarak, Service Registration yazıldı. Bu sayede DI Container a tek tek ekleme yapmak zorunda kalmayacağız.

---
+++ Core/CarBookProject.Application/Services/ApplicationServiceRegistration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarBookProject.Application.Services;
+
+public static class ApplicationServiceRegistration
+{
+    public static void AddApplicationService(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ApplicationServiceRegistration).Assembly));
+    }
+}
