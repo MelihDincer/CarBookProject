f2d6ceeec286d5fe160a1f6a2c9bc268e6cafbce
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-12 18:52:48 +0300

    Service entity için Mediator Design Pattern ve MediatR kütüphanesi kullanılarak Crud işlemleri tamamlandı. API testleri yapıldı.

---
+++ Core/CarBookProject.Application/Features/Mediator/Handlers/ServiceHandlers/GetServiceQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using CarBookProject.Application.Features.Mediator.Queries.ServiceQueries;
+using CarBookProject.Application.Features.Mediator.Results.ServiceResults;
+using CarBookProject.Application.Interfaces;
+using CarBookProject.Domain.Entities;
+using MediatR;
+
+namespace CarBookProject.Application.Features.Mediator.Handlers.ServiceHandlers;
+
+public class GetServiceQueryHandler : IRequestHandler<GetServiceQuery, List<GetServiceQueryResult>>
+{
+    private readonly IRepository<Service> _repository;
+
+    public GetServiceQueryHandler(IRepository<Service> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<List<GetServiceQueryResult>> Handle(GetServiceQuery request, CancellationToken cancellationToken)
+    {
+        var values = await _repository.GetAllAsync();
+        return values.Select(x => new GetServiceQueryResult
+        {
+            Description = x.Description,
+            IconUrl = x.IconUrl,
+            ServiceID = x.ServiceID,
+            Title = x.Title
+        }).ToList();
+    }
+}
---
+++ Core/CarBookProject.Application/Features/Mediator/Handlers/ServiceHandlers/UpdateServiceCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using CarBookProject.Application.Features.Mediator.Commands.ServiceCommands;
+using CarBookProject.Application.Interfaces;
+using CarBookProject.Domain.Entities;
+using MediatR;
+
+namespace CarBookProject.Application.Features.Mediator.Handlers.ServiceHandlers;
+
+public class UpdateServiceCommandHandler : IRequestHandler<UpdateServiceCommand>
+{
+    private readonly IRepository<Service> _repository;
+
+    public UpdateServiceCommandHandler(IRepository<Service> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task Handle(UpdateServiceCommand request, CancellationToken cancellationToken)
+    {
+        var value = await _repository.GetByIdAsync(request.ServiceID);
+        value.Description = request.Description;
+        value.Title = request.Title;
+        value.IconUrl = request.IconUrl;
+        await _repository.UpdateAsync(value);
+    }
+}
