9533c4e55ae66fdeb9c9ffccc8c11afb3a01a036
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-12 12:17:25 +0300

    FooterAddress entity için Mediator Design Pattern ve MediatR kütüphanesi kullanılarak Crud işlemleri tamamlandı. API testleri yapıldı.

---
+++ Core/CarBookProject.Application/Features/Mediator/Handlers/FooterAddressHandlers/RemoveFooterAddressCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using CarBookProject.Application.Features.Mediator.Commands.FooterAddressCommands;
+using CarBookProject.Application.Interfaces;
+using CarBookProject.Domain.Entities;
+using MediatR;
+
+namespace CarBookProject.Application.Features.Mediator.Handlers.FooterAddressHandlers;
+
+public class RemoveFooterAddressCommandHandler : IRequestHandler<RemoveFooterAddressCommand>
+{
+    private readonly IRepository<FooterAddress> _repository;
+
+    public RemoveFooterAddressCommandHandler(IRepository<FooterAddress> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task Handle(RemoveFooterAddressCommand request, CancellationToken cancellationToken)
+    {
+        var value = await _repository.GetByIdAsync(request.Id);
+        await _repository.RemoveAsync(value);
+    }
+}
---
+++ Core/CarBookProject.Application/Features/Mediator/Handlers/FooterAddressHandlers/UpdateFooterAddressCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using CarBookProject.Application.Features.Mediator.Commands.FooterAddressCommands;
+using CarBookProject.Application.Interfaces;
+using CarBookProject.Domain.Entities;
+using MediatR;
+
+namespace CarBookProject.Application.Features.Mediator.Handlers.FooterAddressHandlers;
+
+public class UpdateFooterAddressCommandHandler : IRequestHandler<UpdateFooterAddressCommand>
+{
+    private readonly IRepository<FooterAddress> _repository;
+
+    public UpdateFooterAddressCommandHandler(IRepository<FooterAddress> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task Handle(UpdateFooterAddressCommand request, CancellationToken cancellationToken)
+    {
+        var value = await _repository.GetByIdAsync(request.FooterAddressID);
+        value.Phone = request.Phone;
+        value.Address = request.Address;
+        value.Description = request.Description;
+        value.Email = request.Email;
+        await _repository.UpdateAsync(value);
+    }
+}
