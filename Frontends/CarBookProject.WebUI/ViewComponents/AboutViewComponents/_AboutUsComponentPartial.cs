99dd54555c8e3df36a4b2abfb304978f96de040d
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-14 15:49:46 +0300

    Hakkımda sayfası için bölümlerin viewcomponent olarak ayrılması sağlandı.

---
+++ Frontends/CarBookProject.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using CarBookProject.Dto.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBookProject.WebUI.ViewComponents.AboutViewComponents
+{
+    public class _AboutUsComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AboutUsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7063/api/Abouts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); //responseMessage içerisinden gelen api deki veriyi string formatta oku.
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);// jsonData dan gelen veriyi deserilize et.
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
