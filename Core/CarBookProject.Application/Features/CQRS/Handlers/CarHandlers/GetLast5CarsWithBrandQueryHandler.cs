ded01e42f94836c7649bc72ad00456d83c8c4cce
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-16 19:16:06 +0300

    Son 5 aracın listelenebilmesi için QueryHandler oluşturuldu.

---
+++ Core/CarBookProject.Application/Features/CQRS/Handlers/CarHandlers/GetLast5CarsWithBrandQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using CarBookProject.Application.Features.CQRS.Results.CarResults;
+using CarBookProject.Application.Interfaces.CarInterfaces;
+
+namespace CarBookProject.Application.Features.CQRS.Handlers.CarHandlers;
+
+public class GetLast5CarsWithBrandQueryHandler 
+{
+    private readonly ICarRepository _repository;
+
+    public GetLast5CarsWithBrandQueryHandler(ICarRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public List<GetLast5CarsWithBrandQueryResult> Handle()
+    {
+        var values = _repository.GetLast5CarsWithBrands();
+        return values.Select(x=> new GetLast5CarsWithBrandQueryResult
+        {
+            CarID = x.CarID,
+            BrandName = x.Brand.Name,
+            Model = x.Model,
+            Seat = x.Seat,
+            Transmission = x.Transmission,
+            Fuel = x.Fuel,
+            Km = x.Km,
+            Luggage = x.Luggage,
+            CoverImageUrl = x.CoverImageUrl,
+            BigImageUrl = x.BigImageUrl
+        }).ToList();
+    }
+}
