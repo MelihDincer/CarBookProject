7cd0ba6bdc022e6de5b58071632c401b03b47be7
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-17 14:00:46 +0300

    Author entity için Mediator Design Patter kullanılarak QueryResult ların oluşturulması.

---
+++ Core/CarBookProject.Application/Features/Mediator/Results/AuthorResults/GetAuthorQueryResult.cs
@@ -0,0 +1,9 @@
+﻿namespace CarBookProject.Application.Features.Mediator.Results.AuthorResults;
+
+public class GetAuthorQueryResult
+{
+    public int AuthorID { get; set; }
+    public string Name { get; set; }
+    public string ImageUrl { get; set; }
+    public string Description { get; set; }
+}
