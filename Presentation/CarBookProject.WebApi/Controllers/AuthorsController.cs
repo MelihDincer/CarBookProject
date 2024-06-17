1bd633a36a8d268e0388eb6441c53809a8b407ba
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-17 14:02:34 +0300

    Authors Api Controller oluşturularak testlerin tamamlanması.

---
+++ Presentation/CarBookProject.WebApi/Controllers/AuthorsController.cs
@@ -0,0 +1,53 @@
+﻿using CarBookProject.Application.Features.Mediator.Commands.AuthorCommands;
+using CarBookProject.Application.Features.Mediator.Queries.AuthorQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthorsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AuthorList()
+        {
+            return Ok(await _mediator.Send(new GetAuthorQuery()));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAuthor(int id)
+        {
+            return Ok(await _mediator.Send(new GetAuthorByIdQuery(id)));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAuthor(CreateAuthorCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Yazar başarıyla eklendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveAuthor(int id)
+        {
+            await _mediator.Send(new RemoveAuthorCommand(id));
+            return Ok("Yazar başarıyla silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAuthor(UpdateAuthorCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Yazar başarıyla güncellendi");
+        }
+    }
+}
