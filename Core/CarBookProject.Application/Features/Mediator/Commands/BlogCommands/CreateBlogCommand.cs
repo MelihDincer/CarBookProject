8d76b5cd2555db21edbd51e95e12a2575fb38d75
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-17 15:18:45 +0300

    Blog entity için Command ların yazılması.

---
+++ Core/CarBookProject.Application/Features/Mediator/Commands/BlogCommands/CreateBlogCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace CarBookProject.Application.Features.Mediator.Commands.BlogCommands;
+
+public class CreateBlogCommand : IRequest
+{
+    public string Title { get; set; }
+    public int AuthorID { get; set; }
+    public string CoverImageUrl { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public int CategoryID { get; set; }
+}
