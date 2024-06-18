6580345ff42e235dfca4520458d6f19b18ca597f
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-18 12:04:08 +0300

    Son 3 blog listesinin yazar adı ile listelenmesi için Mediator Tasarım Deseni kullanılarak Query oluşturuldu.

---
+++ Core/CarBookProject.Application/Features/Mediator/Queries/BlogQueries/GetLast3BlogsWithAuthorQuery.cs
@@ -0,0 +1,8 @@
+﻿using CarBookProject.Application.Features.Mediator.Results.BlogResults;
+using MediatR;
+
+namespace CarBookProject.Application.Features.Mediator.Queries.BlogQueries;
+
+public class GetLast3BlogsWithAuthorQuery : IRequest<List<GetLast3BlogsWithAuthorQueryResult>>
+{
+}
