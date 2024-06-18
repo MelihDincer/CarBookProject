f99ae7cdfdd1f67452c1624b4cc4a0f6903d1fd8
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-18 12:04:29 +0300

    Son 3 blog listesinin yazar adı ile listelenmesi için Mediator Tasarım Deseni kullanılarak QueryHandler oluşturuldu.

---
+++ Core/CarBookProject.Application/Features/Mediator/Handlers/BlogHandlers/GetLast3BlogsWithAuthorQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using CarBookProject.Application.Features.Mediator.Queries.BlogQueries;
+using CarBookProject.Application.Features.Mediator.Results.BlogResults;
+using CarBookProject.Application.Interfaces.BlogInterfaces;
+using MediatR;
+
+namespace CarBookProject.Application.Features.Mediator.Handlers.BlogHandlers;
+
+public class GetLast3BlogsWithAuthorQueryHandler : IRequestHandler<GetLast3BlogsWithAuthorQuery, List<GetLast3BlogsWithAuthorQueryResult>>
+{
+    private readonly IBlogRepository _repository;
+
+    public GetLast3BlogsWithAuthorQueryHandler(IBlogRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<List<GetLast3BlogsWithAuthorQueryResult>> Handle(GetLast3BlogsWithAuthorQuery request, CancellationToken cancellationToken)
+    {
+        var values = _repository.GetLast3BlogsWithAuthor();
+        return values.Select(x => new GetLast3BlogsWithAuthorQueryResult
+        {
+            Title = x.Title,
+            AuthorName = x.Author.Name,
+            CategoryName = x.Category.Name,
+            CoverImageUrl = x.CoverImageUrl,
+            BlogID = x.BlogID,
+            CreatedDate = x.CreatedDate
+        }).ToList();
+    }
+}
