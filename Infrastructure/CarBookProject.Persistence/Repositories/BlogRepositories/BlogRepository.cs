5dae62e5bc3905b9266d957dba8c0610723f3bdf
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-18 12:02:39 +0300

    BlogRepository sınıfı oluşturuldu. Son 3 blog listesinin yazar adı ile listelenmesi için tanımlanan metot imzası implemente edilip metot içerisi dolduruldu.

---
+++ Infrastructure/CarBookProject.Persistence/Repositories/BlogRepositories/BlogRepository.cs
@@ -0,0 +1,27 @@
+﻿using CarBookProject.Application.Interfaces.BlogInterfaces;
+using CarBookProject.Domain.Entities;
+using CarBookProject.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarBookProject.Persistence.Repositories.BlogRepositories;
+
+public class BlogRepository : IBlogRepository
+{
+    private readonly CarBookContext _context;
+
+    public BlogRepository(CarBookContext context)
+    {
+        _context = context;
+    }
+
+    public List<Blog> GetLast3BlogsWithAuthor()
+    {
+        var values = _context.Blogs
+            .Include(x => x.Author)
+            .Include(y => y.Category)
+            .OrderByDescending(z => z.BlogID)
+            .Take(3)
+            .ToList();
+        return values;
+    }
+}
