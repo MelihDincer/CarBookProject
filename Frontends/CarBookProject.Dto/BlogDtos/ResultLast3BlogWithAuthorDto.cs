aa4a0bfd902b2d063deb27594718a64888f173ec
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-18 12:28:43 +0300

    ResultLast3BlogWithAuthorDto oluşturuldu.

---
+++ Frontends/CarBookProject.Dto/BlogDtos/ResultLast3BlogWithAuthorDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CarBookProject.Dto.BlogDtos;
+
+public class ResultLast3BlogWithAuthorDto
+{
+    public int BlogID { get; set; }
+    public string Title { get; set; }
+    public string AuthorName { get; set; }
+    public string CoverImageUrl { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string CategoryName { get; set; }
+}
