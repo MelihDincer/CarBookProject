913fb25ed68f6aa149e16d9de6c735e86d5ae1c8
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-18 13:32:10 +0300

    ResultCarPricingWithCarDto oluşturuldu.

---
+++ Frontends/CarBookProject.Dto/CarPricingDtos/ResultCarPricingWithCarDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CarBookProject.Dto.CarPricingDtos;
+
+public class ResultCarPricingWithCarDto
+{
+	public int CarPricingID { get; set; }
+	public string BrandName { get; set; }
+	public string Model { get; set; }
+	public string CoverImageUrl { get; set; }
+	public string PricingName { get; set; }
+	public decimal Amount { get; set; }
+}
