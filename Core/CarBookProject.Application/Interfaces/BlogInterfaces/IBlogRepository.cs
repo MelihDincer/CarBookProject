d3d8b94f641e19ae754e7f1f533aa6ffc89ebbf8
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-18 12:01:54 +0300

    IBlogRepository interface oluşturuldu. Son 3 blog listesinin yazar adı ile listelenmesi için metot imzası tanımlandı.

---
+++ Core/CarBookProject.Application/Interfaces/BlogInterfaces/IBlogRepository.cs
@@ -0,0 +1,8 @@
+﻿using CarBookProject.Domain.Entities;
+
+namespace CarBookProject.Application.Interfaces.BlogInterfaces;
+
+public interface IBlogRepository
+{
+    List<Blog> GetLast3BlogsWithAuthor();
+}
