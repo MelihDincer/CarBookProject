e26198342b56d6defb2f5e9b9893bc70b6de1e42
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-18 12:13:27 +0300

    GetCarPricingWithCarQuery sınıfı oluşturuldu.

---
+++ Core/CarBookProject.Application/Features/Mediator/Queries/CarPricingQueries/GetCarPricingWithCarQuery.cs
@@ -0,0 +1,8 @@
+﻿using CarBookProject.Application.Features.Mediator.Results.CarPricingResults;
+using MediatR;
+
+namespace CarBookProject.Application.Features.Mediator.Queries.CarPricingQueries;
+
+public class GetCarPricingWithCarQuery : IRequest<List<GetCarPricingWithCarQueryResult>>
+{
+}
