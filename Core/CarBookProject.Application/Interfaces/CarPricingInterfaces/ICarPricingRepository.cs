dd493254eaff728597f742f5b9958f52862e6d41
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-18 12:11:51 +0300

    Araba fiyatlandırma listesinde günlük kiralama verisi olan arabaların listelenmesi için ICarPricingRepository interface oluşturularak metot imzası tanımlandı.

---
+++ Core/CarBookProject.Application/Interfaces/CarPricingInterfaces/ICarPricingRepository.cs
@@ -0,0 +1,8 @@
+﻿using CarBookProject.Domain.Entities;
+
+namespace CarBookProject.Application.Interfaces.CarPricingInterfaces;
+
+public interface ICarPricingRepository
+{
+	Task<List<CarPricing>> GetCarPricingWithCarsAsync();
+}
