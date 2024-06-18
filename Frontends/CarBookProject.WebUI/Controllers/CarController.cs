fd02e3fa119a16330692072c230ba914b38b8903
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-18 13:37:14 +0300

    Arabaların listelendiği bölüm revize edildi. Araçlar pricing ile listeleneceği için apiye istek atılan yer ve dto güncellendi.

--- Frontends/CarBookProject.WebUI/Controllers/CarController.cs
+++ Frontends/CarBookProject.WebUI/Controllers/CarController.cs
@@ -1,4 +1,5 @@
 ﻿using CarBookProject.Dto.CarDtos;
+using CarBookProject.Dto.CarPricingDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -15,14 +16,14 @@ namespace CarBookProject.WebUI.Controllers
 
         public async Task<IActionResult> Index()
         {
-            ViewBag.v1 = "Araçlar";
-            ViewBag.v2 = "Araçlarımız";
+            ViewBag.v1 = "Araclar";
+            ViewBag.v2 = "Mevcut Araçlarımız";
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7063/api/Cars/GetCarWithBrand");
+            var responseMessage = await client.GetAsync("https://localhost:7063/api/CarPricings");
             if(responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandDto>>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<ResultCarPricingWithCarDto>>(jsonData);
                 return View(values);
             }
             return View();
