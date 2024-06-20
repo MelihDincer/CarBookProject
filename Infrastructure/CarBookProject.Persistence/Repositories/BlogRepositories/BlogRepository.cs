e39cee6f77f58ad614266e207978ed7c04cb3d83
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-20 18:12:39 +0300

    Blog entity e özgü tanımlanan metot kodları yazıldı. Bu sınıftaki iki metot da asenkron olarak revize edildi. Asenkron yapılan metotlar sayesinde Handle metotlarındaki uyarının giderilmesi sağlandı.

--- Infrastructure/CarBookProject.Persistence/Repositories/BlogRepositories/BlogRepository.cs
+++ Infrastructure/CarBookProject.Persistence/Repositories/BlogRepositories/BlogRepository.cs
@@ -7,21 +7,31 @@ namespace CarBookProject.Persistence.Repositories.BlogRepositories;
 
 public class BlogRepository : IBlogRepository
 {
-    private readonly CarBookContext _context;
+	private readonly CarBookContext _context;
 
-    public BlogRepository(CarBookContext context)
-    {
-        _context = context;
-    }
+	public BlogRepository(CarBookContext context)
+	{
+		_context = context;
+	}
 
-    public List<Blog> GetLast3BlogsWithAuthor()
-    {
-        var values = _context.Blogs
-            .Include(x => x.Author)
-            .Include(y => y.Category)
-            .OrderByDescending(z => z.BlogID)
-            .Take(3)
-            .ToList();
-        return values;
-    }
+	public Task<List<Blog>> GetAllBlogsWithAuthorAsync()
+	{
+		var values = _context.Blogs
+			.Include(x => x.Author)
+			.Include(y => y.Category)
+			.OrderByDescending(z => z.BlogID)
+			.ToListAsync();
+		return values;
+	}
+
+	public Task<List<Blog>> GetLast3BlogsWithAuthorAsync()
+	{
+		var values = _context.Blogs
+			.Include(x => x.Author)
+			.Include(y => y.Category)
+			.OrderByDescending(z => z.BlogID)
+			.Take(3)
+			.ToListAsync();
+		return values;
+	}
 }
