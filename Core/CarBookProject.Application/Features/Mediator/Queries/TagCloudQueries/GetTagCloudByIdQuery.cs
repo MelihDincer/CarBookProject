2a553c16d2fd676529e766d3182e3c1b802fcb11
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-21 20:05:11 +0300

    TagCloud entity için querylerin oluşturulması.

---
+++ Core/CarBookProject.Application/Features/Mediator/Queries/TagCloudQueries/GetTagCloudByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using CarBookProject.Application.Features.Mediator.Results.TagCloudResults;
+using MediatR;
+
+namespace CarBookProject.Application.Features.Mediator.Queries.TagCloudQueries;
+
+public class GetTagCloudByIdQuery : IRequest<GetTagCloudByIdQueryResult>
+{
+    public int Id { get; set; }
+
+    public GetTagCloudByIdQuery(int id)
+    {
+        Id = id;
+    }
+}
---
+++ Core/CarBookProject.Application/Features/Mediator/Queries/TagCloudQueries/GetTagCloudQuery.cs
@@ -0,0 +1,8 @@
+﻿using CarBookProject.Application.Features.Mediator.Results.TagCloudResults;
+using MediatR;
+
+namespace CarBookProject.Application.Features.Mediator.Queries.TagCloudQueries;
+
+public class GetTagCloudQuery : IRequest<List<GetTagCloudQueryResult>>
+{ 
+}
