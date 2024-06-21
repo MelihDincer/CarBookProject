b8e3772a37e66bc84b8e9f2bc2da85bb8ebfbb60
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-21 20:06:15 +0300

    Son 3 blogun blog detay sayfasındaki son bloglar kısmında listelenmesi işlemi tamamlandı.

--- Frontends/CarBookProject.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsRecentBlogsComponentPartial.cs
+++ Frontends/CarBookProject.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsRecentBlogsComponentPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBookProject.Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBookProject.WebUI.ViewComponents.BlogViewComponents;
 
 public class _BlogDetailsRecentBlogsComponentPartial : ViewComponent
 {
-    public IViewComponentResult Invoke()
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _BlogDetailsRecentBlogsComponentPartial(IHttpClientFactory httpClientFactory)
     {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7063/api/Blogs/GetLast3BlogsWithAuthorList");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultLast3BlogWithAuthorDto>>(jsonData);
+            return View(values);
+        }
         return View();
     }
 }
