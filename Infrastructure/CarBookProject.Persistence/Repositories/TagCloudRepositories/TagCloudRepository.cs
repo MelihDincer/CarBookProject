badd09f3640ebc778df74e9e2ac824fc2e6b6f39
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-22 17:11:36 +0300

    TagCloudRepository sınıfı oluşturuldu. Tag Cloudların blog id sine göre listelenebilmesi için metot yazıldı.

---
+++ Infrastructure/CarBookProject.Persistence/Repositories/TagCloudRepositories/TagCloudRepository.cs
@@ -0,0 +1,22 @@
+﻿using CarBookProject.Application.Interfaces.TagCloudInterfaces;
+using CarBookProject.Domain.Entities;
+using CarBookProject.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarBookProject.Persistence.Repositories.TagCloudRepositories;
+
+public class TagCloudRepository : ITagCloudRepository
+{
+    private readonly CarBookContext _context;
+
+    public TagCloudRepository(CarBookContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<TagCloud>> GetTagCloudsByBlogIdAsync(int id)
+    {
+        var values = await _context.TagClouds.Where(x=>x.BlogID == id).ToListAsync();
+        return values;
+    }
+}
