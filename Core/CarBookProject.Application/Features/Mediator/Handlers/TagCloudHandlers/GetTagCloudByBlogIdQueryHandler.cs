4093c8daa29ed2990a923ef7f18c409849f90402
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-22 17:10:19 +0300

    GetTagCloudByBlogIdQueryHandler oluşturuldu.

---
+++ Core/CarBookProject.Application/Features/Mediator/Handlers/TagCloudHandlers/GetTagCloudByBlogIdQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using CarBookProject.Application.Features.Mediator.Queries.TagCloudQueries;
+using CarBookProject.Application.Features.Mediator.Results.TagCloudResults;
+using CarBookProject.Application.Interfaces.TagCloudInterfaces;
+using MediatR;
+
+namespace CarBookProject.Application.Features.Mediator.Handlers.TagCloudHandlers;
+
+public class GetTagCloudByBlogIdQueryHandler : IRequestHandler<GetTagCloudByBlogIdQuery, List<GetTagCloudByBlogIdQueryResult>>
+{
+    private readonly ITagCloudRepository _repository;
+
+    public GetTagCloudByBlogIdQueryHandler(ITagCloudRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<List<GetTagCloudByBlogIdQueryResult>> Handle(GetTagCloudByBlogIdQuery request, CancellationToken cancellationToken)
+    {
+        var values = await _repository.GetTagCloudsByBlogIdAsync(request.Id);
+        return values.Select(x => new GetTagCloudByBlogIdQueryResult
+        {
+            BlogID = x.BlogID,
+            Name = x.Name,
+            TagCloudID = x.TagCloudID
+        }).ToList();
+    }
+}
