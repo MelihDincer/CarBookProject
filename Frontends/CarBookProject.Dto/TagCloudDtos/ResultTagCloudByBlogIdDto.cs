d84c715c73a23381373ca628d7accc646861f45d
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-22 17:14:50 +0300

    ResultTagCloudByBlogIdDto oluşturuldu.

---
+++ Frontends/CarBookProject.Dto/TagCloudDtos/ResultTagCloudByBlogIdDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CarBookProject.Dto.TagCloudDtos;
+
+public class ResultTagCloudByBlogIdDto
+{
+    public int TagCloudId { get; set; }
+    public int BlogId { get; set; }
+    public string Name { get; set; }
+
+}
