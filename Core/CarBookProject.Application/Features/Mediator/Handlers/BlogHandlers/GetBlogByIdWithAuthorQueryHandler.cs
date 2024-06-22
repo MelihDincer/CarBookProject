9b2a330e2c2aea5e8489b3132762e1e98160f28e
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-22 17:09:56 +0300

    GetBlogByIdWithAuthorQueryHandler oluşturuldu.

---
+++ Core/CarBookProject.Application/Features/Mediator/Handlers/BlogHandlers/GetBlogByIdWithAuthorQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using CarBookProject.Application.Features.Mediator.Queries.BlogQueries;
+using CarBookProject.Application.Features.Mediator.Results.BlogResults;
+using CarBookProject.Application.Interfaces.BlogInterfaces;
+using MediatR;
+
+namespace CarBookProject.Application.Features.Mediator.Handlers.BlogHandlers;
+
+public class GetBlogByIdWithAuthorQueryHandler : IRequestHandler<GetBlogByIdWithAuthorQuery, GetBlogByIdWithAuthorQueryResult>
+{
+    private readonly IBlogRepository _repository;
+
+    public GetBlogByIdWithAuthorQueryHandler(IBlogRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<GetBlogByIdWithAuthorQueryResult> Handle(GetBlogByIdWithAuthorQuery request, CancellationToken cancellationToken)
+    {
+        var value = await _repository.GetBlogByIdWithAuthorAsync(request.Id);
+        return new GetBlogByIdWithAuthorQueryResult
+        {
+            BlogID = value.BlogID,
+            AuthorDescription = value.Author.Description,
+            AuthorImageUrl = value.Author.ImageUrl,
+            AuthorName = value.Author.Name
+        };
+    }
+}
