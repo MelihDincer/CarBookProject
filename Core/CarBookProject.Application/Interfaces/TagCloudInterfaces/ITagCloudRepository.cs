cf7362a7a736b1779d48d85bfe1d694904074bde
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-22 17:11:18 +0300

    ITagCloudRepository oluşturuldu. Tag Cloudların blog id sine göre listelenebilmesi için metot imzası tanımlandı.

---
+++ Core/CarBookProject.Application/Interfaces/TagCloudInterfaces/ITagCloudRepository.cs
@@ -0,0 +1,8 @@
+﻿using CarBookProject.Domain.Entities;
+
+namespace CarBookProject.Application.Interfaces.TagCloudInterfaces;
+
+public interface ITagCloudRepository
+{
+    Task<List<TagCloud>> GetTagCloudsByBlogIdAsync(int id);
+}
