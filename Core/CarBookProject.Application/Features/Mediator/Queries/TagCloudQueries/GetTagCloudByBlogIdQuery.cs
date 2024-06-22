bf2ad36708acfa8089d3979e47a1bc41542eada0
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-22 17:10:27 +0300

    GetTagCloudByBlogIdQuery oluşturuldu.

---
+++ Core/CarBookProject.Application/Features/Mediator/Queries/TagCloudQueries/GetTagCloudByBlogIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using CarBookProject.Application.Features.Mediator.Results.TagCloudResults;
+using MediatR;
+
+namespace CarBookProject.Application.Features.Mediator.Queries.TagCloudQueries;
+
+public class GetTagCloudByBlogIdQuery : IRequest<List<GetTagCloudByBlogIdQueryResult>>
+{
+    public int Id { get; set; }
+
+    public GetTagCloudByBlogIdQuery(int id)
+    {
+        Id = id;
+    }
+}
