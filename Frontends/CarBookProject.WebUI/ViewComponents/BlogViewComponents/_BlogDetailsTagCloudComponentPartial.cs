2fc3cc00c3684b3d4138914eff016662fe4b1c77
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-22 17:26:54 +0300

    Etiket bulutlarının blog id ye göre ilgili blogun detay sayfasında gösterilme işlemi tamamlandı.

--- Frontends/CarBookProject.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsTagCloudComponentPartial.cs
+++ Frontends/CarBookProject.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsTagCloudComponentPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBookProject.Dto.TagCloudDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBookProject.WebUI.ViewComponents.BlogViewComponents;
 
 public class _BlogDetailsTagCloudComponentPartial : ViewComponent
 {
-    public IViewComponentResult Invoke()
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _BlogDetailsTagCloudComponentPartial(IHttpClientFactory httpClientFactory)
     {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7063/api/TagClouds");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultTagCloudDto>>(jsonData);
+            return View(values);
+        }
         return View();
     }
 }
