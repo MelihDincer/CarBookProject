76a13f15debd4649ae7dcdc3ede77b415da81031
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-22 17:09:41 +0300

    GetBlogByIdWithAuthorQuery oluşturuldu.

---
+++ Core/CarBookProject.Application/Features/Mediator/Queries/BlogQueries/GetBlogByIdWithAuthorQuery.cs
@@ -0,0 +1,14 @@
+﻿using CarBookProject.Application.Features.Mediator.Results.BlogResults;
+using MediatR;
+
+namespace CarBookProject.Application.Features.Mediator.Queries.BlogQueries;
+
+public class GetBlogByIdWithAuthorQuery : IRequest<GetBlogByIdWithAuthorQueryResult>
+{   
+    public int Id { get; set; }
+
+    public GetBlogByIdWithAuthorQuery(int id)
+    {
+        Id = id;
+    }
+}
