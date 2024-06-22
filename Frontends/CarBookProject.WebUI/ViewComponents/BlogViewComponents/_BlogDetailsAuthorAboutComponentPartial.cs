0e6ae5dacf912d86d239eec4e6a303fa0afb2716
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-22 17:21:15 +0300

    Blog Id ye göre ilgili bloğu yazan yazarın bilgilerinin apiden çekilmesi.

--- Frontends/CarBookProject.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsAuthorAboutComponentPartial.cs
+++ Frontends/CarBookProject.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsAuthorAboutComponentPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBookProject.Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBookProject.WebUI.ViewComponents.BlogViewComponents;
 
 public class _BlogDetailsAuthorAboutComponentPartial : ViewComponent
 {
-    public IViewComponentResult Invoke()
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _BlogDetailsAuthorAboutComponentPartial(IHttpClientFactory httpClientFactory)
     {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync(int id)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync($"https://localhost:7063/api/Blogs/GetBlogWithAuthor?id={id}");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var value = JsonConvert.DeserializeObject<ResultBlogByIdDto>(jsonData);
+            return View(value);
+        }
         return View();
     }
 }
