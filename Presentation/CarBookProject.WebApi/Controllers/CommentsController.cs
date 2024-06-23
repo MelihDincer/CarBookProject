7662655f947e2e1d5d4189a639224a66607be248
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-23 22:16:19 +0300

    Repository Pattern örneği için Comments Controller oluşturuldu. CRUD işlemleri yapıldı. Burada ilerleyen vakitlerde revizeye gidilecek.

---
+++ Presentation/CarBookProject.WebApi/Controllers/CommentsController.cs
@@ -0,0 +1,51 @@
+﻿using CarBookProject.Application.Features.RepositoryPattern;
+using CarBookProject.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookProject.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CommentsController : ControllerBase
+{
+    private readonly IGenericRepository<Comment> _commentRepository;
+
+    public CommentsController(IGenericRepository<Comment> commentRepository)
+    {
+        _commentRepository = commentRepository;
+    }
+
+    [HttpGet]
+    public IActionResult CommentList()
+    {
+        var values = _commentRepository.GetAll();
+        return Ok(values);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetComment(int id)
+    {
+        return Ok(_commentRepository.GetById(id));
+    }
+
+    [HttpPost]
+    public IActionResult CreateComment(Comment comment)
+    {
+        _commentRepository.Add(comment);
+        return Ok("Yorum başarıyla eklendi.");
+    }
+
+    [HttpDelete]
+    public IActionResult DeleteComment(int id)
+    {
+        _commentRepository.Remove(new Comment { CommentID = id });
+        return Ok("Yorum başarıyla silindi.");
+    }
+
+    [HttpPut]
+    public IActionResult Update(Comment comment)
+    {
+        _commentRepository.Update(comment);
+        return Ok("Yorum başarıyla eklendi.");
+    }   
+}
