8972921e54ba17a842b3e9463023c38eaba347f5
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-23 22:15:40 +0300

    Repository Design pattern kullanımı örneği olarak IGenericRepository den implemente edilen Comment entity için bir repository oluşturuldu.

---
+++ Infrastructure/CarBookProject.Persistence/Repositories/CommentRepositories/CommentRepository.cs
@@ -0,0 +1,44 @@
+﻿using CarBookProject.Application.Features.RepositoryPattern;
+using CarBookProject.Domain.Entities;
+using CarBookProject.Persistence.Context;
+
+namespace CarBookProject.Persistence.Repositories.CommentRepositories;
+
+public class CommentRepository<T> : IGenericRepository<Comment>
+{
+    private readonly CarBookContext _context;
+
+    public CommentRepository(CarBookContext context)
+    {
+        _context = context;
+    }
+
+    public void Add(Comment entity)
+    {
+        _context.Add(entity);
+        _context.SaveChanges();
+    }
+
+    public List<Comment> GetAll()
+    {
+        return _context.Comments.ToList();
+    }
+
+    public Comment GetById(int id)
+    {
+        return _context.Comments.Find(id);
+    }
+
+    public void Remove(Comment entity)
+    {
+        var value = _context.Comments.Find(entity.CommentID);
+        _context.Remove(value);
+        _context.SaveChanges();
+    }
+
+    public void Update(Comment entity)
+    {
+        _context.Update(entity);
+        _context.SaveChanges();
+    }
+}
