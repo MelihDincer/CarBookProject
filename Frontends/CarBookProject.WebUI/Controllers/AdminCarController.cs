6507dae65a2eead32f69e77ef4c6dcf9dcb25467
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-24 23:25:25 +0300

    AdminCarController oluşturuldu.

---
+++ Frontends/CarBookProject.WebUI/Controllers/AdminCarController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookProject.WebUI.Controllers;
+
+public class AdminCarController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    
+}
