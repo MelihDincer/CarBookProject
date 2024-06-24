74e81188db92e3f3cef8e2d5c219e10a99235bb6
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-24 22:54:17 +0300

    AdminLayout Controller oluşturuldu. Index sayfası layout olarak kullanılacak şekilde ilerlenildi. Header ve navbar bölümleri için partial view oluşturmak için action metotlar tanımlandı.

---
+++ Frontends/CarBookProject.WebUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookProject.WebUI.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AdminHeaderPartial()
+        {
+            return View();
+        }
+
+        public IActionResult AdminNavbarPartial()
+        {
+            return View();
+        }
+    }
+}
