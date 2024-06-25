4feba1916de7f67d6f6021d1c1c704b0f6b286c0
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-25 23:08:46 +0300

    AdminCar Controller da araba bilgilerinin tabloya çekilmesi için api consume işlemi yapıldı.

--- Frontends/CarBookProject.WebUI/Controllers/AdminCarController.cs
+++ Frontends/CarBookProject.WebUI/Controllers/AdminCarController.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBookProject.Dto.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBookProject.WebUI.Controllers;
 
 public class AdminCarController : Controller
 {
-    public IActionResult Index()
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public AdminCarController(IHttpClientFactory httpClientFactory)
     {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var response = await client.GetAsync("https://localhost:7063/api/Cars/GetCarWithBrand");
+        if(response.IsSuccessStatusCode)
+        {
+            var jsonData = await response.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandDto>>(jsonData);
+            return View(values);
+        }
         return View();
     }
 
