7e83b556af9ebaff834c0a5171e14d4e639f7a77
Melih DİNÇER <m_dincer41@hotmail.com>
2024-06-30 11:17:06 +0300

    Araba özellikleri tablosu (listeleme) ve yeni özellik ekleme işlemleri tamamlandı.

---
+++ Frontends/CarBookProject.Dto/FeatureDtos/CreateFeatureDto.cs
@@ -0,0 +1,6 @@
+﻿namespace CarBookProject.Dto.FeatureDtos;
+
+public class CreateFeatureDto
+{
+    public string Name { get; set; }
+}
---
+++ Frontends/CarBookProject.WebUI/Controllers/AdminFeatureController.cs
@@ -0,0 +1,50 @@
+﻿using CarBookProject.Dto.FeatureDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarBookProject.WebUI.Controllers
+{
+    public class AdminFeatureController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminFeatureController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7063/api/Features");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateFeature()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFeature(ResultFeatureDto resultFeatureDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(resultFeatureDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7063/api/Features", stringContent);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
