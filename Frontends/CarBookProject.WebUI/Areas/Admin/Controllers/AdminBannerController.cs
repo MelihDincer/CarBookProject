0883af00ea9d8b4d66f1d7f52dd681fbb0668113
Melih DİNÇER <m_dincer41@hotmail.com>
2024-07-04 00:34:41 +0300

    Admin Banner bölümünün Area kullanılarak oluşturulması.

---
+++ Frontends/CarBookProject.WebUI/Areas/Admin/Controllers/AdminBannerController.cs
@@ -0,0 +1,30 @@
+﻿using CarBookProject.Dto.BannerDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBookProject.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminBannerController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminBannerController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7063/api/Banners");
+            if(response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBannerDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
