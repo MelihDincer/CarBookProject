8420e728eaac705e3217af6febf82200962dceed
Melih DİNÇER <m_dincer41@hotmail.com>
2024-07-04 17:25:03 +0300

    Admin Banner bölümünde yeni banner alanı ekleme işlemleri başarıyla tamamlandı.

--- Frontends/CarBookProject.WebUI/Areas/Admin/Controllers/AdminBannerController.cs
+++ Frontends/CarBookProject.WebUI/Areas/Admin/Controllers/AdminBannerController.cs
@@ -1,30 +1,51 @@
 ﻿using CarBookProject.Dto.BannerDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 
-namespace CarBookProject.WebUI.Areas.Admin.Controllers
+namespace CarBookProject.WebUI.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Route("Admin/[controller]/[action]/{id?}")]
+public class AdminBannerController : Controller
 {
-    [Area("Admin")]
-    public class AdminBannerController : Controller
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public AdminBannerController(IHttpClientFactory httpClientFactory)
     {
-        private readonly IHttpClientFactory _httpClientFactory;
+        _httpClientFactory = httpClientFactory;
+    }
 
-        public AdminBannerController(IHttpClientFactory httpClientFactory)
+    public async Task<IActionResult> Index()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var response = await client.GetAsync("https://localhost:7063/api/Banners");
+        if (response.IsSuccessStatusCode)
         {
-            _httpClientFactory = httpClientFactory;
+            var jsonData = await response.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultBannerDto>>(jsonData);
+            return View(values);
         }
+        return View();
+    }
 
-        public async Task<IActionResult> Index()
+    [HttpGet]
+    public IActionResult CreateBanner()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateBanner(CreateBannerDto createBannerDto)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var jsonData = JsonConvert.SerializeObject(createBannerDto);
+        StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        var response = await client.PostAsync("https://localhost:7063/api/Banners", stringContent);
+        if (response.IsSuccessStatusCode)
         {
-            var client = _httpClientFactory.CreateClient();
-            var response = await client.GetAsync("https://localhost:7063/api/Banners");
-            if(response.IsSuccessStatusCode)
-            {
-                var jsonData = await response.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultBannerDto>>(jsonData);
-                return View(values);
-            }
-            return View();
+            return RedirectToAction("Index");
         }
+        return View();
     }
 }
