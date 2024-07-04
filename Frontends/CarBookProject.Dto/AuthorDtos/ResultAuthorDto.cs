53d96408e7641ecb7acbd8d1a6557dc7169d7205
Melih DİNÇER <m_dincer41@hotmail.com>
2024-07-04 20:27:41 +0300

    Admin Author (Yazar) bölümünün tamamlanması.

---
+++ Frontends/CarBookProject.Dto/AuthorDtos/ResultAuthorDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CarBookProject.Dto.AuthorDtos;
+
+public class ResultAuthorDto
+{
+    public int AuthorID { get; set; }
+    public string Name { get; set; }
+    public string ImageUrl { get; set; }
+    public string Description { get; set; }
+}
---
+++ Frontends/CarBookProject.WebUI/Areas/Admin/Controllers/AuthorController.cs
@@ -0,0 +1,91 @@
+﻿using CarBookProject.Dto.AuthorDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarBookProject.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]/{id?}")]
+    public class AdminAuthorController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminAuthorController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7063/api/Authors");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAuthorDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateAuthor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAuthor(CreateAuthorDto createAuthorDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createAuthorDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7063/api/Authors", stringContent);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> RemoveAuthor(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.DeleteAsync($"https://localhost:7063/api/Authors?id={id}");
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateAuthor(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync($"https://localhost:7063/api/Authors/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<UpdateAuthorDto>(jsonData);
+                return View(value);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateAuthor(UpdateAuthorDto updateAuthorDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateAuthorDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response = await client.PutAsync("https://localhost:7063/api/Authors", stringContent);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
