7140815c5cb1b1bf8bedcc3058788d28f243860a
Melih DİNÇER <m_dincer41@hotmail.com>
2024-07-04 18:22:23 +0300

    Admin paneli Banner bölümünün tamamlanması.

--- Frontends/CarBookProject.WebUI/Areas/Admin/Controllers/AdminBannerController.cs
+++ Frontends/CarBookProject.WebUI/Areas/Admin/Controllers/AdminBannerController.cs
@@ -1,6 +1,7 @@
 ﻿using CarBookProject.Dto.BannerDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Net.Http;
 using System.Text;
 
 namespace CarBookProject.WebUI.Areas.Admin.Controllers;
@@ -42,6 +43,45 @@ public class AdminBannerController : Controller
         var jsonData = JsonConvert.SerializeObject(createBannerDto);
         StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
         var response = await client.PostAsync("https://localhost:7063/api/Banners", stringContent);
+        if (response.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+
+    public async Task<IActionResult> RemoveBanner(int id)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var response = await client.DeleteAsync($"https://localhost:7063/api/Banners?id={id}");
+        if (response.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> UpdateBanner(int id)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var response = await client.GetAsync($"https://localhost:7063/api/Banners/{id}");
+        if (response.IsSuccessStatusCode)
+        {
+            var jsonData = await response.Content.ReadAsStringAsync();
+            var value = JsonConvert.DeserializeObject<UpdateBannerDto>(jsonData);
+            return View(value);
+        }
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UpdateBanner(UpdateBannerDto updateBannerDto)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var jsonData = JsonConvert.SerializeObject(updateBannerDto);
+        StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        var response = await client.PutAsync("https://localhost:7063/api/Banners", stringContent);
         if (response.IsSuccessStatusCode)
         {
             return RedirectToAction("Index");
