9a9051983650cef5bf43150aa31b90deaef4c2bd
Melih DİNÇER <m_dincer41@hotmail.com>
2024-07-05 23:51:24 +0300

    Blog yorumları bölümü için Mediator pattern ile Querylerin oluşturulması.

---
+++ Core/CarBookProject.Application/Features/Mediator/Queries/CommentQueries/GetCommentByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using CarBookProject.Application.Features.Mediator.Results.CommentResults;
+using MediatR;
+
+namespace CarBookProject.Application.Features.Mediator.Queries.CommentQueries;
+
+public class GetCommentByIdQuery : IRequest<GetCommentByIdWithBlogQueryResult>
+{
+    public int Id { get; set; }
+
+    public GetCommentByIdQuery(int id)
+    {
+        Id = id;
+    }
+}
---
+++ Core/CarBookProject.Application/Features/Mediator/Queries/CommentQueries/GetCommentQuery.cs
@@ -0,0 +1,8 @@
+﻿using CarBookProject.Application.Features.Mediator.Results.CommentResults;
+using MediatR;
+
+namespace CarBookProject.Application.Features.Mediator.Queries.CommentQueries;
+
+public class GetCommentQuery : IRequest<List<GetAllCommentWithBlogQueryResult>>
+{
+}
