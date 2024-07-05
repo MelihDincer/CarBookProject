bc8978e96f141917828eac64a119e99eac1305a1
Melih DİNÇER <m_dincer41@hotmail.com>
2024-07-05 23:55:59 +0300

    UpdateCommentCommandHandler oluşturularak güncelleme işlemi yapıldı.

---
+++ Core/CarBookProject.Application/Features/Mediator/Handlers/CommentHandlers/UpdateCommentCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using CarBookProject.Application.Features.Mediator.Commands.CommentCommands;
+using CarBookProject.Application.Interfaces;
+using CarBookProject.Domain.Entities;
+using MediatR;
+
+namespace CarBookProject.Application.Features.Mediator.Handlers.CommentHandlers;
+
+public class UpdateCommentCommandHandler : IRequestHandler<UpdateCommentCommand>
+{
+    private readonly IRepository<Comment> _repository;
+
+    public UpdateCommentCommandHandler(IRepository<Comment> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task Handle(UpdateCommentCommand request, CancellationToken cancellationToken)
+    {
+        var value = await _repository.GetByIdAsync(request.CommentID);
+        value.Description = request.Description;
+        value.Name = request.Name;
+        value.Description = request.Description;
+        value.BlogID = request.BlogID;
+        await _repository.UpdateAsync(value);
+    }
+}
