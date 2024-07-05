55daad81b291e87f0b77ab44ac3a7df11113511e
Melih DİNÇER <m_dincer41@hotmail.com>
2024-07-05 23:55:26 +0300

    CreateCommentCommandHandler oluşturularak ekleme işlemi yapıldı.

---
+++ Core/CarBookProject.Application/Features/Mediator/Handlers/CommentHandlers/CreateCommentCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using CarBookProject.Application.Features.Mediator.Commands.CommentCommands;
+using CarBookProject.Application.Interfaces;
+using CarBookProject.Domain.Entities;
+using MediatR;
+
+namespace CarBookProject.Application.Features.Mediator.Handlers.CommentHandlers;
+
+public class CreateCommentCommandHandler : IRequestHandler<CreateCommentCommand>
+{
+    private readonly IRepository<Comment> _repository;
+
+    public CreateCommentCommandHandler(IRepository<Comment> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task Handle(CreateCommentCommand request, CancellationToken cancellationToken)
+    {
+        await _repository.CreateAsync(new Comment
+        {
+            BlogID = request.BlogID,
+            CreatedDate = DateTime.UtcNow,
+            Description = request.Description,
+            Name = request.Name
+        });
+    }
+}
