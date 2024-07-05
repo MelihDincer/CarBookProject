8f51dbf44619795ecffd1d68cedf69f9f1ae72ff
Melih DİNÇER <m_dincer41@hotmail.com>
2024-07-05 23:55:45 +0300

    RemoveCommentCommandHandler oluşturularak silme işlemi yapıldı.

---
+++ Core/CarBookProject.Application/Features/Mediator/Handlers/CommentHandlers/RemoveCommentCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using CarBookProject.Application.Features.Mediator.Commands.CommentCommands;
+using CarBookProject.Application.Interfaces;
+using CarBookProject.Domain.Entities;
+using MediatR;
+
+namespace CarBookProject.Application.Features.Mediator.Handlers.CommentHandlers;
+
+public class RemoveCommentCommandHandler : IRequestHandler<RemoveCommentCommand>
+{
+    private readonly IRepository<Comment> _repository;
+
+    public RemoveCommentCommandHandler(IRepository<Comment> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task Handle(RemoveCommentCommand request, CancellationToken cancellationToken)
+    {
+        var value = await _repository.GetByIdAsync(request.Id);
+        await _repository.RemoveAsync(value);
+    }
+}
