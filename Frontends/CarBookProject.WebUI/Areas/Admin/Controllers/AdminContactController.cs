10539e23eab0145fde7864c3bcd59f46f672ca83
Melih DİNÇER <m_dincer41@hotmail.com>
2024-07-08 23:50:29 +0300

    UI Admin Contact tarafında Index sayfasının oluşturulup yorum listesinin bu sayfa üzerinden görüntülenmesi işlemi tamamlandı.

---
+++ Frontends/CarBookProject.WebUI/Areas/Admin/Controllers/AdminContactController.cs
@@ -0,0 +1,31 @@
+﻿using CarBookProject.Dto.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBookProject.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]/{id?}")]
+    public class AdminContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7063/api/Contacts");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultContactDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
