78b8ac433490e8c2ca4e0074536493dfd9a3e571
Melih DİNÇER <m_dincer41@hotmail.com>
2025-11-07 23:29:35 +0300

    Lokasyon seçimi için lokasyonlarımızı listeleme işlemi ve dropdown ekleme işlemleri tamamlandı.

--- Frontends/CarBookProject.WebUI/Controllers/DefaultController.cs
+++ Frontends/CarBookProject.WebUI/Controllers/DefaultController.cs
@@ -1,11 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBookProject.Dto.LocationDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 
 namespace CarBookProject.WebUI.Controllers
 {
     public class DefaultController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7063/api/Locations");
+
+            var jsonData = await response.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultLocationDto>>(jsonData);
+            List<SelectListItem> values2 = (from x in values
+                                            select new SelectListItem
+                                            {
+                                                Text = x.Name,
+                                                Value = x.LocationID.ToString()                                               
+                                            }).ToList();
+            ViewBag.v = values2;
             return View();
         }
     }
