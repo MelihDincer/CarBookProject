0a511f47032c7724844f04d620f7ee2e87ad7383
Melih DİNÇER <m_dincer41@hotmail.com>
2025-11-16 01:32:06 +0300

    Yorum yapan görselini AD/SOYAD görseli şeklinde ekleme işlemi yapıldı.

--- Frontends/CarBookProject.Dto/CommentDtos/ResultCommentDto.cs
+++ Frontends/CarBookProject.Dto/CommentDtos/ResultCommentDto.cs
@@ -10,4 +10,16 @@ public class ResultCommentDto
     public string AuthorName { get; set; }
     public string AuthorImageUrl { get; set; }
     public string CategoryName { get; set; }
+
+    public string NameInitials
+    {
+        get
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+                return "";
+
+            var parts = Name.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            return string.Join("", parts.Select(x => x[0])).Substring(0, Math.Min(2, parts.Count())).ToUpper();
+        }
+    }
 }
