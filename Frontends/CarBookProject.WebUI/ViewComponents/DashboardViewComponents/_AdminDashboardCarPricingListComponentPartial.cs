5fa60073f2fb008135d462b6ccdd0cb3f6173a1b
Melih DİNÇER <m_dincer41@hotmail.com>
2025-11-22 23:57:27 +0300

    Dashboardda araç kiralama fiyat listesi eklendi.

---
+++ Frontends/CarBookProject.WebUI/ViewComponents/DashboardViewComponents/_AdminDashboardCarPricingListComponentPartial.cs
@@ -0,0 +1,32 @@
+﻿using CarBookProject.Dto.CarPricingDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace CarBookProject.WebUI.ViewComponents.DashboardViewComponents
+{
+    public class _AdminDashboardCarPricingListComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AdminDashboardCarPricingListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ViewBag.v1 = "Paketler";
+            ViewBag.v1 = "Araç Fiyat Paketleri";
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7063/api/CarPricings/GetCarPricingWithTimePeriod");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarPricingWithTimePeriod>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
