1f89a36f6a11f2c7bd3257896e26f3a47f7476fe
Melih DİNÇER <m_dincer41@hotmail.com>
2025-11-23 00:51:19 +0300

    Dashboardda son eklenen 5 blog listesi eklendi.

---
+++ Frontends/CarBookProject.WebUI/ViewComponents/DashboardViewComponents/_AdminDashboardBlogListComponentPartial.cs
@@ -0,0 +1,31 @@
+﻿using CarBookProject.Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace CarBookProject.WebUI.ViewComponents.DashboardViewComponents
+{
+    public class _AdminDashboardBlogListComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AdminDashboardBlogListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7063/api/Blogs/GetAllBlogsWithAuthorList");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAllBlogsWithAuthorDto>>(jsonData);
+                var last5Values = values.OrderByDescending(x=>x.BlogID).Take(5).ToList();
+                return View(last5Values);
+            }
+            return View();
+        }
+    }
+}
