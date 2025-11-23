70b1bf78d75bcc587f270e9cc0916cba2fd2f983
Melih DİNÇER <m_dincer41@hotmail.com>
2025-11-23 19:32:54 +0300

    Araba özellikleri, car details kısmında özellikler tab pane ine eklendi.

---
+++ Frontends/CarBookProject.WebUI/ViewComponents/CarDetailViewComponents/_CarDetailCarFeatureByCarIdComponentPartial.cs
@@ -0,0 +1,31 @@
+﻿using CarBookProject.Dto.CarFeatureDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace CarBookProject.WebUI.ViewComponents.CarDetailViewComponents
+{
+    public class _CarDetailCarFeatureByCarIdComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _CarDetailCarFeatureByCarIdComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            ViewBag.carId = id;
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7063/api/CarFeatures?carId={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarFeatureByCarIdDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
